f649a6e867851c0f2d1765a407649a7ceb42f84a
jjonami <nickelcomplex@gmail.com>
2021-01-15 17:29:21 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JJONAMI on 2021/01/15.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
